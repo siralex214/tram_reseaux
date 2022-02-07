ba96908d5f0111c307e854a9580b86cc71e235ef
alexis Mouchon <alexismouchon09@gmail.com>
2022-02-07 16:48:22 +0100

    base de données récupéré

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+
 ?>
 
 <!doctype html>
@@ -9,10 +10,12 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Nom-du-site | Page d'accueil |</title>
+
+    <script src="./assets/script.js" defer></script>
 </head>
 <body>
 <?php include_once "./inclu/header.php" ?>
-
+<input id="ajout" type="button" value="ajouter">
 <main>
 
 </main>
