046937fd66ba549c7248dcec6e921a45c5c892ba
Ousbane Houda <82388160+ousbahou@users.noreply.github.com>
2022-02-08 15:36:55 +0100

    navbar V2

--- inclu/header.php
+++ inclu/header.php
@@ -1,14 +1,34 @@
 <header>
-    <div id="nav">
-        <div id="triangle">
-            <img src="../style/logo_novalink.png" id="logo" alt="" width="68px">
-        </div>
-        <div id="ligne1">
-        </div>
-        <h2>Qui sommes-nous ?</h2>
-        <div id="ligne2">
 
+    <div id="nav">
+
+        <div id="triangle">
+            <img src="./assets/img/logo_novalink.png" id="logo" alt="" width="68px">
         </div>
-        <h2>Accueil</h2>
+
+        <div id="ligne1"></div>
+        <div id="ligne2"></div>
+        <div id="ligne3"></div>
+        <div id="ligne4"></div>
+
+        <div id="rond1"></div>
+        <div id="cercle1"></div>
+
     </div>
+
+    <h2 id="quisommesnous">Qui sommes-nous ?</h2>
+    <h2 id="accueil">Accueil</h2>
+
+    <div id="tourne"></div>
+    <div id="tourner"></div>
+
+    <div id="point_lumineux">
+        <p>&nbsp;</p>
+    </div>
+    <div id="point_lumineux2">
+        <p>&nbsp;</p>
+    </div>
+
+    </div>
+
 </header>
---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+?>
+
+<!doctype html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link rel="stylesheet" href="./assets/style/style.css">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Reem+Kufi&display=swap" rel="stylesheet">
+    <title> NOVALINK | Page d'accueil |</title>
+</head>
+
+<body>
+    <?php include_once "./inclu/header.php" ?>
+    <main>
+
+    </main>
+    <?php include_once "./inclu/footer.php" ?>
+
+</body>
+
+</html>
