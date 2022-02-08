b9039978991c8fb832d639d088f930706bf2fa8c
alexis Mouchon <alexismouchon09@gmail.com>
2022-02-08 15:30:45 +0100

    modal termninée

--- inclu/header.php
+++ inclu/header.php
@@ -1,7 +1,7 @@
 <header>
     <div id="nav">
         <div id="triangle">
-            <img src="../style/logo_novalink.png" id="logo" alt="" width="68px">
+            <img src="./assets/img/logo_novalink.png" id="logo" alt="" width="68px">
         </div>
         <div id="ligne1">
         </div>
@@ -10,5 +10,29 @@
 
         </div>
         <h2>Accueil</h2>
+        <input id="button" type='checkbox'>
+        <label id="button_label">Click Me!</label>
+        <div class='modal'>
+            <i id="fermeture_connexion" class="fas fa-times"></i>
+            <div class="container">
+                <div class="front side">
+                    <div class="content">
+
+                        <button class="vers_inscription">Pas encore inscrit, <span style="color: red"> bah faite le</span></button>
+                    </div>
+                </div>
+                <div class="back side">
+                    <div class="content">
+
+                        <button  class="vers_inscription">Ta déjà un compte <span style="color: red">bah connecte toi batard</span></button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+    </div>
+
+
     </div>
 </header>
---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+?>
+
+<!doctype html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link rel="stylesheet" href="assets/style/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="./assets/script/script.js" defer></script>
+    <title> NOVALINK | Page d'accueil |</title>
+</head>
+
+<body>
+<?php include_once "./inclu/header.php" ?>
+
+<main>
+</main>
+<?php include_once "./inclu/footer.php" ?>
+
+</body>
+
+</html>
