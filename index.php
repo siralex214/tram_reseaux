140489400065b63dac61786f0b66bd7dd2b77d07
alexis Mouchon <alexismouchon09@gmail.com>
2022-02-08 09:30:38 +0100

    fin ajout BDD

--- index.php
+++ index.php
@@ -11,11 +11,10 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Nom-du-site | Page d'accueil |</title>
 
-    <script src="./assets/script.js" defer></script>
+    <script src="assets/script/script.js" defer></script>
 </head>
 <body>
 <?php include_once "./inclu/header.php" ?>
-<input id="ajout" type="button" value="ajouter">
 <main>
 
 </main>
