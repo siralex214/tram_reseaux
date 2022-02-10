06b53e9c569b67a472b27f0e1577ca15cac021fa
Ousbane Houda <82388160+ousbahou@users.noreply.github.com>
2022-02-10 14:56:54 +0100

    commit pdo

--- inclu/pdo.php
+++ inclu/pdo.php
@@ -2,7 +2,7 @@
 
 
 try {
-    $pdo = new PDO('mysql:host=localhost;dbname=reseau_tram', "root", "", array(
+    $pdo = new PDO('mysql:host=localhost;dbname=reseau_tram', "root", "root", array(
         PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
         PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING
