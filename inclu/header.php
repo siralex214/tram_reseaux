f295a39efdb13ff49cf26a3eab4957a01a43600c
Ousbane Houda <82388160+ousbahou@users.noreply.github.com>
2022-02-10 14:24:50 +0100

    deplacement header

--- inclu/header.php
+++ inclu/header.php
@@ -1,19 +1,4 @@
 
-<!doctype html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Reem+Kufi&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="assets/style/style.css">
-    <script src="./assets/script/script.js" defer></script>
-    <title> NOVALINK | Page d'accueil |</title>
-    <link rel="icon" type="image/x-icon" href="./assets/img/favicon.png">
-</head>
 <header>
     <div id="nav">
 
