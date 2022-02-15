c2d0db1eb813d893e8f16eee6f39de7883ed07c4
Fabsend <97021007+Fabsend@users.noreply.github.com>
2022-02-15 17:01:20 +0100

    image dash

--- user/accueil_user.php
+++ user/accueil_user.php
@@ -22,67 +22,40 @@ require_once '../inclu/pdo.php';
     <?php include_once "./inclu/header.php"; ?>
     <main>
 
-        <div id="centre_graph">
-            <div id="logo_graph">
-                <a href=""><img src="../assets/img/maison_bouton.png" width="50%" alt="accueil"></a>
-                <a href=""><img src="../assets/img/homme_bouton.png" width="50%" alt="homme"></a>
-                <a href=""><img src="../assets/img/maison_bouton.png" width="50%" alt=""></a>
-                <a href=""><img src="../assets/img/maison_bouton.png" width="50%" alt=""></a>
-                <a href=""><img src="../assets/img/maison_bouton.png" width="50%" alt=""></a>
-            </div>
-            <div id="nom_graph">
-                <div class="bouton_graph"></div>
-                <div class="bouton_graph"></div>
-            </div>
-            <div id="place_graph">
-
-            </div>
-        </div>
-
-        <div class="labox">
-
-            <section id="Allchart">
-                <section class="one_graph">
-                    <h2>Protocol de connexion</h2>
-                    <div style="width: 400px; height: 400px">
-                        <canvas id="graph1">
-                        </canvas>
-                    </div>
-                </section>
+        <section id="Allchart">
+            <section class="one_graph">
+                <h2>Protocol de connexion</h2>
+                <canvas id="graph1">
+                </canvas>
+                </div>
             </section>
-            <section id="chart2">
-                <section class="two_graph">
-                    <h2>etat des serveurs</h2>
-                    <div style="width: 400px; height: 400px">
-                        <canvas id="graph2">
+        </section>
+        <section id="chart2">
+            <section class="two_graph">
+                <h2>etat des serveurs</h2>
+                <canvas id="graph2">
 
-                        </canvas>
-                    </div>
-                </section>
+                </canvas>
+                </div>
             </section>
-            <section id="chart3">
-                <section class="trois_graph">
-                    <h2>Port de depart</h2>
-                    <div style="width: 400px; height: 400px">
-                        <canvas id="graph3">
+        </section>
+        <section id="chart3">
+            <section class="trois_graph">
+                <h2>Port de depart</h2>
+                <canvas id="graph3">
 
-                        </canvas>
-                    </div>
-                </section>
+                </canvas>
+                </div>
             </section>
-            <section id="chart4">
-                <section class="quatre_graph">
-                    <h2>Port de arriver</h2>
-                    <div style="width: 400px; height: 400px">
-                        <canvas id="graph4">
+        </section>
+        <section id="chart4">
+            <section class="quatre_graph">
+                <h2>Port de arriver</h2>
+                <canvas id="graph4">
 
-                        </canvas>
-                    </div>
-                </section>
-        </div>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-            <path fill="#279b8f" fill-opacity="1" d="M0,32L80,69.3C160,107,320,181,480,181.3C640,181,800,107,960,69.3C1120,32,1280,32,1360,32L1440,32L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path>
-        </svg>
+                </canvas>
+                </div>
+            </section>
     </main>
     <?php include_once "../inclu/footer.php" ?>
 </body>
