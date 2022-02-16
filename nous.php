8c0e45abbbaaff124e651f788b3b73b80b223c31
alexis Mouchon <alexismouchon09@gmail.com>
2022-02-16 15:36:56 +0100

    branch a jour

--- nous.php
+++ nous.php
@@ -10,33 +10,39 @@ $errors = [];
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./assets/style/style.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="assets/style/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
+          integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg=="
+          crossorigin="anonymous" referrerpolicy="no-referrer"/>
     <script src="./assets/script/script.js" defer></script>
     <link rel="icon" type="image/x-icon" href="./assets/img/favicon.png">
     <title>NovaLink | Qui sommes-nous ? </title>
 </head>
 
 <body>
-    <?php include_once "./inclu/header.php" ?>
+<?php include_once "./inclu/header.php" ?>
 
-    <main class="main_qsn">
-        <section class="section_qsn">
-    <div class="container_h1_qsn">
-        <h1 class="h1-qsn">Qui sommes-nous ?</h1>
-    </div>
-         
-
-            <div class="intro_qsn">
-                <p>Nova Link est une entreprise d'analyse de réseau organisationnel, nous mesurons et traçons les modèles de collaboration en examinant la force, la fréquence et la nature des interactions entre les personnes dans les réseaux. Nova Link fournit des informations détaillées sur différents facteurs, tels que le flux d'informations, la prise de décision, l'optimisation de revenus, l'innovation, l'inclusion, la confiance et l'énergie.</p>
-            </div>
+<main class="main_qsn">
+    <section class="section_qsn">
+        <div class="container_h1_qsn">
+            <h1 class="h1-qsn">Qui sommes-nous ?</h1>
+        </div>
 
 
-            <div class="container">
-                <div class="picture"><span><img class="img_qsn" src="./assets/img/laptop_novalink.png" alt=""></span></div>
+        <div class="intro_qsn">
+            <p>Nova Link est une entreprise d'analyse de réseau organisationnel, nous mesurons et traçons les modèles de
+                collaboration en examinant la force, la fréquence et la nature des interactions entre les personnes dans
+                les réseaux. Nova Link fournit des informations détaillées sur différents facteurs, tels que le flux
+                d'informations, la prise de décision, l'optimisation de revenus, l'innovation, l'inclusion, la confiance
+                et l'énergie.</p>
+        </div>
 
 
-                <div class="text_qsn">
+        <div class="container">
+            <div class="picture"><span><img class="img_qsn" src="./assets/img/laptop_novalink.png" alt=""></span></div>
+
+
+            <div class="text_qsn">
                     <span>
                         <h2>Nova Link en chiffres</h2>
                         <div class="sub_container">
@@ -79,13 +85,13 @@ $errors = [];
 
                         </div>
                     </span>
-                </div>
             </div>
-            <!-- <p>Nous continuons de nous surpasser et d'innover pour vous proposez une qualité irréprochable.</p> -->
+        </div>
+        <!-- <p>Nous continuons de nous surpasser et d'innover pour vous proposez une qualité irréprochable.</p> -->
 
 
-            <div class="container">
-                <div class="text_qsn2"><span>
+        <div class="container">
+            <div class="text_qsn2"><span>
                         <ul>
                             <li>Appel gratuit</li>
                             <li>Support disponible 24h/24 et 7j/7</li>
@@ -94,100 +100,103 @@ $errors = [];
 
                     </ul>
                 </span></div>
-                <div class="picture2"><span><img class="img_qsn" src="./assets/img/support_novalink.png" alt=""></span></div>
+            <div class="picture2"><span><img class="img_qsn" src="./assets/img/support_novalink.png" alt=""></span>
             </div>
+        </div>
 
 
-            <div class="container">
-                <div class="picture"><span><img class="img_qsn" src="./assets/img/settings_novalink.png" alt=""></span></div>
-                <div class="text_qsn"><span>Une entreprise d'analyse réseau est précieuse, avec des outils de gestion des incidents natifs, Nova Link permet à vos équipes d'agir immédiatement et d'automatiser le processus de résolution.</span></div>
+        <div class="container">
+            <div class="picture"><span><img class="img_qsn" src="./assets/img/settings_novalink.png" alt=""></span>
             </div>
-
-
-
-            <div class="container">
-                <div class="text_qsn2"><span>Nova Link soulage bon nombre des difficultés courantes auxquelles les professionnels de l'informatique sont confrontés dans leurs tâches quotidiennes et lorsqu'ils sont de garde, dans le but d'éviter l'épuisement professionnel et la monotonie.</span></div>
-                <div class="picture2"><span><img class="img_qsn" src="./assets/img/financial_novalink.png" alt=""></span></div>
+            <div class="text_qsn"><span>Une entreprise d'analyse réseau est précieuse, avec des outils de gestion des incidents natifs, Nova Link permet à vos équipes d'agir immédiatement et d'automatiser le processus de résolution.</span>
             </div>
-            <section class="blue">
-                <h2><span class="effect_blue"> NOVA</span> </h2>
-                <p>Nos valeurs nous animent tous les jours, nous connectent, inspirent et nous permettent d'aller de l'avant.</p>
+        </div>
 
-                <div class="container_card_qsn js_card_qsn">
-                    <div class="card_qsn1"><span>
+
+        <div class="container">
+            <div class="text_qsn2"><span>Nova Link soulage bon nombre des difficultés courantes auxquelles les professionnels de l'informatique sont confrontés dans leurs tâches quotidiennes et lorsqu'ils sont de garde, dans le but d'éviter l'épuisement professionnel et la monotonie.</span>
+            </div>
+            <div class="picture2"><span><img class="img_qsn" src="./assets/img/financial_novalink.png" alt=""></span>
+            </div>
+        </div>
+        <section class="blue">
+            <h2><span class="effect_blue"> NOVA</span></h2>
+            <p>Nos valeurs nous animent tous les jours, nous connectent, inspirent et nous permettent d'aller de
+                l'avant.</p>
+
+            <div class="container_card_qsn js_card_qsn">
+                <div class="card_qsn1"><span>
                             <h3><span class="n">N</span>ovateur</h3>
                             <p>Nous travaillons ensemble pour élargir et poursuivre l'évolution sur les domaines d'intérêt existants et nouveaux.</p>
                         </span></div>
-                    <div class="card_qsn2"><span>
+                <div class="card_qsn2"><span>
                             <h3><span class="o">O</span>rganisé</h3>
                             <p>Nous sommes conscient que le temps est précieux, l'organisation est la clé de la réussite.</p>
                         </span></div>
-                    <div class="card_qsn3"><span>
+                <div class="card_qsn3"><span>
                             <h3><span class="v">V</span>igilant</h3>
                             <p>Nous avons pensé à tout pour nous assurer que la surveillance soit sécurisée et précise.</p>
-                        </span> </div>
-                    <div class="card_qsn4"><span>
+                        </span></div>
+                <div class="card_qsn4"><span>
                             <h3><span class="a">A</span>mbitieux</h3>
                             <p>Nous mettons un point d'honneur à nous dépasser et à toujours faire mieux qu'hier car votre satisfaction est notre objectif.</p>
                         </span></div>
-                </div>
+            </div>
 
-                <section class="red">
-                    <h2>Les entreprises qui nous font confiance</h2>
+            <section class="red">
+                <h2>Les entreprises qui nous font confiance</h2>
 
 
-                    <div class="container_card_qsn js_logo_qsn">
-                        <div class="card_red"><span>
+                <div class="container_card_qsn js_logo_qsn">
+                    <div class="card_red"><span>
 
                                 <p><img src="./assets/img/abestergo.png" alt=""></p>
                             </span></div>
-                        <div class="card_red"><span>
+                    <div class="card_red"><span>
 
                                 <p><img class="fox" src="./assets/img/fox.png" alt=""></p>
                             </span></div>
-                        <div class="card_red"><span>
+                    <div class="card_red"><span>
 
                                 <p><img src="./assets/img/umbrella.png" alt=""></p>
-                            </span> </div>
-                        <div class="card_red">
+                            </span></div>
+                    <div class="card_red">
                             <span>
                                 <p>
                                     <img class="fallout" src="./assets/img/fallout.png" alt="">
                                 </p>
                             </span>
-                        </div>
+                    </div>
 
-                        <div class="card_red">
+                    <div class="card_red">
                             <span>
                                 <p>
                                     <img src="./assets/img/shinra.png" alt="">
                                 </p>
                             </span>
-                        </div>
+                    </div>
 
-                        <div class="card_red">
+                    <div class="card_red">
                             <span>
                                 <p>
                                     <img src="./assets/img/capsule.png" alt="">
                                 </p>
                             </span>
-                        </div>
-
                     </div>
 
-
-
-                    <!-- section abonnements -->
-                </section>
-
+                </div>
 
 
                 <!-- section abonnements -->
             </section>
+
+
+            <!-- section abonnements -->
         </section>
-        </section>
-    </main>
-    <?php include_once "./inclu/footer.php" ?>
+    </section>
+    </section>
+</main>
+<?php include_once "./inclu/footer.php" ?>
 </body>
 
 </html>
