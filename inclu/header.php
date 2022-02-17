bee120e8d35f2bd0a1658947ea8590f814917411
Ousbane Houda <82388160+ousbahou@users.noreply.github.com>
2022-02-17 14:46:47 +0100

    header + FOOTER OK (ne pas toucher) sauf alexis

--- inclu/header.php
+++ inclu/header.php
@@ -1,133 +1,117 @@
 <header>
-    <div id="nav">
-        <!-- MODAL BOUTON CONNECTION -->
-        <input id="button" type='checkbox'>
-
-        <label for="switch" class="button" id="button_label">SE CONNECTER</label>
-
-        <div class='modal'>
-            <div>
-                <i id="fermeture_connexion" class="fas fa-times"></i>
-                <div class="container_modal">
-                    <div class="front side">
-                        <div class="content">
-                            <h1>Connexion</h1>
-                            <div id="content_connexion">
-                                <img id="img_connexion" src="../assets/img/lock_novalink.png" alt="img_connexion">
-
-                                <form class="form_connexion" action="" method="POST">
-
-                                    <div class="col-3">
-                                        <input id="email_connexion" class="effect-2" type="text" placeholder="E-mail">
-                                        <span class="focus-border" id="focus_connexion_email"></span>
-                                    </div>
-
-                                    <p class="error" id="erreur_mail_co">&nbsp;</p>
-
-                                    <div class="col-3">
-                                        <input id="password_connexion" class="effect-2" type="text"
-                                               placeholder="Mot de passe">
-                                        <span class="focus-border" id="focus_connexion_password"></span>
-                                    </div>
-                                    <p class="error" id="erreur_password_co">&nbsp;</p>
-                                    <div class="col-3 submit_div">
-                                        <input id="submit_connexion" class="submit_button" type="submit"
-                                               value="connexion" name="submitted">
-                                        <span id="error_connexion" class="error"><?php viewError($errors, "invalid"); ?> &nbsp;</span>
-                                    </div>
+    <img src="./assets/img/logo_novalink.png" alt="logo">
+    <ul>
 
 
-                            </div>
+        <li><a href="./index.php">Accueil</li>
+        <li><a href="./nous.php">Qui sommes-nous ?</a></li>
+    </ul>
+    
+    <input id="button" type='checkbox'>
 
-                            <div class="col-3 last_button">
-                                <div class="vers_inscription">Pas encore inscrit, <span
-                                            style="color: black; text-decoration: underline;"> N'hésite plus!</span>
+    <li><label for="switch" class="button" id="button_label">SE CONNECTER</label></li>
+
+    <div class='modal'>
+        <div>
+            <i id="fermeture_connexion" class="fas fa-times"></i>
+            <div class="container_modal">
+                <div class="front side">
+                    <div class="content">
+                        <h1>Connexion</h1>
+                        <div id="content_connexion">
+                            <img id="img_connexion" src="../assets/img/lock_novalink.png" alt="img_connexion">
+
+                            <form class="form_connexion" action="" method="POST">
+
+                                <div class="col-3">
+                                    <input id="email_connexion" class="effect-2" type="text" placeholder="E-mail">
+                                    <span class="focus-border" id="focus_connexion_email"></span>
                                 </div>
-                            </div>
-                            </form>
+
+                                <p class="error" id="erreur_mail_co">&nbsp;</p>
+
+                                <div class="col-3">
+                                    <input id="password_connexion" class="effect-2" type="text" placeholder="Mot de passe">
+                                    <span class="focus-border" id="focus_connexion_password"></span>
+                                </div>
+                                <p class="error" id="erreur_password_co">&nbsp;</p>
+                                <div class="col-3 submit_div">
+                                    <input id="submit_connexion" class="submit_button" type="submit" value="connexion" name="submitted">
+                                    <span id="error_connexion" class="error"><?php viewError($errors, "invalid"); ?> &nbsp;</span>
+                                </div>
+
+
                         </div>
 
+                        <div class="col-3 last_button">
+                            <div class="vers_inscription">Pas encore inscrit, <span style="color: black; text-decoration: underline;"> N'hésite plus!</span>
+                            </div>
+                        </div>
+                        </form>
                     </div>
-                    <!-- INSCRIPTION -->
-                    <div class="back side">
-                        <div class="content">
 
-                            <div class="inscription">
-                                <h1>Inscription</h1>
-                                <form action="" method="post">
-                                    <div style="display: flex; flex-direction: column">
-                                        <div style="display: flex; justify-content: space-evenly">
-                                            <div style="display: flex;flex-direction: column">
-                                                <div class="col-3">
-                                                    <input id="prenom_inscription" class="effect-2" type="text"
-                                                           placeholder="Prénom">
-                                                    <span class="focus-border" id="focus_prenom_inscription"></span>
-                                                </div>
-                                                <span class="error" id="erreur_prenom_inscription">&nbsp;</span>
+                </div>
+                <!-- INSCRIPTION -->
+                <div class="back side">
+                    <div class="content">
+
+                        <div class="inscription">
+                            <h1>Inscription</h1>
+                            <form action="" method="post">
+                                <div style="display: flex; flex-direction: column">
+                                    <div style="display: flex; justify-content: space-evenly">
+                                        <div style="display: flex;flex-direction: column">
+                                            <div class="col-3">
+                                                <input id="prenom_inscription" class="effect-2" type="text" placeholder="Prénom">
+                                                <span class="focus-border" id="focus_prenom_inscription"></span>
                                             </div>
-                                            <div style="display: flex;flex-direction: column">
-                                                <div class="col-3">
-                                                    <input id="nom_inscription" class="effect-2" type="text"
-                                                           placeholder="Nom">
-                                                    <span class="focus-border" id="focus_nom_inscription"></span>
-                                                </div>
-                                                <span class="error" id="erreur_nom_inscription">&nbsp;</span>
+                                            <span class="error" id="erreur_prenom_inscription">&nbsp;</span>
+                                        </div>
+                                        <div style="display: flex;flex-direction: column">
+                                            <div class="col-3">
+                                                <input id="nom_inscription" class="effect-2" type="text" placeholder="Nom">
+                                                <span class="focus-border" id="focus_nom_inscription"></span>
                                             </div>
+                                            <span class="error" id="erreur_nom_inscription">&nbsp;</span>
                                         </div>
                                     </div>
-                                    <div style="display: flex; flex-direction: column; align-items: center;">
-                                        <div style="display: flex;flex-direction: column">
-                                            <div class="col-3" style="width: 500px; margin: 24px auto 0;">
-                                                <input id="email_inscription" class="effect-2" type="text"
-                                                       placeholder="Email">
-                                                <span class="focus-border" id="focus_email_inscription"></span>
-                                            </div>
-                                            <span class="error" id="erreur_mail_inscription">&nbsp;</span>
-                                        </div>
-                                        <div style="display: flex;flex-direction: column">
-                                            <div class="col-3" style="width: 500px; margin: 24px auto 0;">
-                                                <input id="password_inscription" class="effect-2" type="text"
-                                                       placeholder="Mot de passe">
-                                                <span class="focus-border" id="focus_password_inscription"></span>
-                                            </div>
-                                            <span class="error" id="erreur_password_inscription">&nbsp;</span>
-                                        </div>
-                                        <div style="display: flex;flex-direction: column">
-                                            <div class="col-3" style="width: 500px; margin: 24px auto 0;">
-                                                <input id="cgu_inscription" class="effect-2" type="checkbox">
-                                                <label id="label_cgu" for="cgu_inscription">Veuillez accepter les conditions générales d'utilisations</label>
-                                            </div>
-                                        </div>
-                                        <div class="col-3 submit_div">
-                                            <input id="submit_inscription" class="submit_button" type="submit"
-                                                   value="inscription" name="submitted">
-                                            <span id="error_inscription" class="error"> &nbsp;</span>
-                                        </div>
-                                    </div>
-                                </form>
-                                <br>
-                                <div class="vers_inscription">Ta déjà un compte? <span
-                                            style="color: red">Connecte toi!</span>
                                 </div>
+                                <div style="display: flex; flex-direction: column; align-items: center;">
+                                    <div style="display: flex;flex-direction: column">
+                                        <div class="col-3" style="width: 500px; margin: 24px auto 0;">
+                                            <input id="email_inscription" class="effect-2" type="text" placeholder="Email">
+                                            <span class="focus-border" id="focus_email_inscription"></span>
+                                        </div>
+                                        <span class="error" id="erreur_mail_inscription">&nbsp;</span>
+                                    </div>
+                                    <div style="display: flex;flex-direction: column">
+                                        <div class="col-3" style="width: 500px; margin: 24px auto 0;">
+                                            <input id="password_inscription" class="effect-2" type="text" placeholder="Mot de passe">
+                                            <span class="focus-border" id="focus_password_inscription"></span>
+                                        </div>
+                                        <span class="error" id="erreur_password_inscription">&nbsp;</span>
+                                    </div>
+                                    <div style="display: flex;flex-direction: column">
+                                        <div class="col-3" style="width: 500px; margin: 24px auto 0;">
+                                            <input id="cgu_inscription" class="effect-2" type="checkbox">
+                                            <label id="label_cgu" for="cgu_inscription">Veuillez accepter les conditions générales d'utilisations</label>
+                                        </div>
+                                    </div>
+                                    <div class="col-3 submit_div">
+                                        <input id="submit_inscription" class="submit_button" type="submit" value="inscription" name="submitted">
+                                        <span id="error_inscription" class="error"> &nbsp;</span>
+                                    </div>
+                                </div>
+                            </form>
+                            <br>
+                            <div class="vers_inscription">Ta déjà un compte? <span style="color: red">Connecte toi!</span>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-
         </div>
-        <ul>
-            <li><a href="./nous.php">
-                    <h2 id="quisommesnous">Qui sommes-nous ?</h2>
-                </a></li>
-            <a href="./index.php">
-                <h2 id="accueil">Accueil</h2>
-            </a>
-            <a href="./faq.php">
-                <h2 id="faq">FAQ</h2>
-            </a>
-        </ul>
-        
-
     </div>
+
+
 </header>
